010964d36d0467b5ae38a4a2135fed752f57eb7e
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-11-27 21:00:58 +0100

    added boilerplate for POST restaurant review endpoint.

---
+++ src/BurgerHub.Api/Domain/Endpoints/Restaurants/Reviews/PostReviewForRestaurant.cs
@@ -0,0 +1,27 @@
+﻿using Ardalis.ApiEndpoints;
+using BurgerHub.Api.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerHub.Api.Domain.Endpoints.Restaurants.Reviews;
+
+public record PostReviewRequest(
+    [FromRoute] string RestaurantId,
+    [FromBody] ReviewScoresRequest Scores);
+
+public record ReviewScoresRequest(
+    ReviewScore? Texture,
+    ReviewScore? Taste,
+    ReviewScore? Visual);
+
+public class PostReviewForRestaurant : BaseAsyncEndpoint
+    .WithRequest<PostReviewRequest>
+    .WithoutResponse
+{
+    [HttpPost("api/restaurants/{RestaurantId}/review")]
+    public override Task<ActionResult> HandleAsync(
+        [FromRoute] PostReviewRequest request, 
+        CancellationToken cancellationToken = new())
+    {
+        throw new NotImplementedException();
+    }
+}
