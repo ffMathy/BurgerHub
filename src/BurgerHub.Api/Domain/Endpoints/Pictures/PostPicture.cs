f9d93efeadb27ad90872fa4de6e65a563fced5a9
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-11-27 21:23:41 +0100

    added post picture boilerplate.

---
+++ src/BurgerHub.Api/Domain/Endpoints/Pictures/PostPicture.cs
@@ -0,0 +1,25 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerHub.Api.Domain.Endpoints.Pictures;
+
+public record PostPictureRequest(
+    IFormFile File);
+
+public class PostPicture : BaseAsyncEndpoint
+    .WithRequest<PostPictureRequest>
+    .WithoutResponse
+{
+    public PostPicture()
+    {
+        
+    }
+    
+    [HttpPost("api/pictures")]
+    [Authorize]
+    public override Task<ActionResult> HandleAsync(PostPictureRequest request, CancellationToken cancellationToken = new CancellationToken())
+    {
+        throw new NotImplementedException();
+    }
+}
