f21827ed538daf34a230a85083927a0a9028e19f
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-11-27 22:17:02 +0100

    tweak mongo settings.

---
+++ src/BurgerHub.Api/Infrastructure/MongoOptions.cs
@@ -0,0 +1,9 @@
+﻿using Destructurama.Attributed;
+
+namespace BurgerHub.Api.Infrastructure;
+
+public class MongoOptions
+{
+    [NotLogged] public string ConnectionString { get; set; } = null!;
+    [NotLogged] public string DatabaseName { get; set; } = null!;
+}
