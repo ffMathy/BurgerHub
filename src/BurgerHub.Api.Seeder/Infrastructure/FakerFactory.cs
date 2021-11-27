6cb6e683ec16e520a240d14438743247eb10b4ba
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-11-27 23:13:56 +0100

    finalized faker factory.

--- src/BurgerHub.Api.Seeder/Infrastructure/FakerFactory.cs
+++ src/BurgerHub.Api.Seeder/Infrastructure/FakerFactory.cs
@@ -1,7 +1,9 @@
 ﻿using Bogus;
+using Bogus.DataSets;
 using BurgerHub.Api.Domain.Models;
 using BurgerHub.Api.Infrastructure.Encryption;
 using MongoDB.Bson;
+using MongoDB.Driver.GeoJsonObjectModel;
 
 namespace BurgerHub.Api.Seeder.Infrastructure;
 
@@ -47,16 +49,61 @@ public class FakerFactory : IFakerFactory
 
     public Faker<Review> CreateReviewFaker()
     {
-        throw new NotImplementedException();
+        return new Faker<Review>()
+            .StrictMode(true)
+            .RuleFor(
+                x => x.AuthorUserId,
+                ObjectId.GenerateNewId)
+            .RuleForType(
+                typeof(ReviewScores),
+                x => new ReviewScores()
+                {
+                    Taste = x.Random.Enum<ReviewScore>(),
+                    Texture = x.Random.Enum<ReviewScore>(),
+                    Visual = x.Random.Enum<ReviewScore>()
+                });
     }
 
     public Faker<Photo> CreatePhotoFaker()
     {
-        throw new NotImplementedException();
+        return new Faker<Photo>()
+            .StrictMode(true)
+            .RuleFor(
+                x => x.Id,
+                ObjectId.GenerateNewId)
+            .RuleFor(
+                x => x.Url,
+                x => x.Image.PlaceImgUrl(
+                    category: PlaceImgCategory.Architecture))
+            .Ignore(
+                x => x.AuthorUserId);
     }
 
     public Faker<Restaurant> CreateRestaurantFaker()
     {
-        throw new NotImplementedException();
+        return new Faker<Restaurant>()
+            .StrictMode(true)
+            .RuleFor(
+                x => x.Name,
+                x => x.Company.CompanyName())
+            .RuleFor(
+                x => x.Location,
+                x => new GeoJsonPoint<GeoJson2DGeographicCoordinates>(
+                    new GeoJson2DGeographicCoordinates(
+                        x.Address.Longitude(),
+                        x.Address.Latitude())))
+            .RuleFor(
+                x => x.OpeningTimes,
+                x => x.Random.ArrayElements(x.Random
+                    .EnumValues<DayOfWeek>()
+                    .Select(dayOfWeek => new OpeningTime(
+                        dayOfWeek,
+                        OpenTimeUtc: x.Date.BetweenTimeOnly(
+                            new TimeOnly(8, 0),
+                            new TimeOnly(11, 0)),
+                        CloseTimeUtc: x.Date.BetweenTimeOnly(
+                            new TimeOnly(16, 0),
+                            new TimeOnly(21, 0))))
+                    .ToArray()));
     }
 }
