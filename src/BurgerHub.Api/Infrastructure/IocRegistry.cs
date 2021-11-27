2da29ed3aa5416632f6d163c2b3b7b4b30c618e4
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-11-27 22:17:31 +0100

    added basic ioc registry with mongo and options registered.

---
+++ src/BurgerHub.Api/Infrastructure/IocRegistry.cs
@@ -0,0 +1,58 @@
+﻿using BurgerHub.Api.Domain.Models;
+using BurgerHub.Api.Infrastructure.Encryption;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace BurgerHub.Api.Infrastructure;
+
+public class IocRegistry
+{
+    private readonly IServiceCollection _serviceCollection;
+
+    public IocRegistry(
+        IServiceCollection serviceCollection)
+    {
+        _serviceCollection = serviceCollection;
+    }
+
+    public void Register()
+    {
+        RegisterOptions();
+        RegisterMongo();
+    }
+
+    private void RegisterOptions()
+    {
+        _serviceCollection.AddOptions<MongoOptions>("Mongo");
+        _serviceCollection.AddOptions<EncryptionOptions>("Encryption");
+    }
+
+    private void RegisterMongo()
+    {
+        _serviceCollection.AddScoped<IMongoDatabase>(provider =>
+        {
+            var options = provider.GetService<IOptions<MongoOptions>>();
+            var connectionString =
+                options?.Value.ConnectionString ??
+                throw new InvalidOperationException("Mongo connection string not found.");
+            
+            var client = new MongoClient(connectionString);
+            return client.GetDatabase(options.Value.DatabaseName);
+        });
+        
+        RegisterMongoCollection<User>();
+        RegisterMongoCollection<Review>();
+        RegisterMongoCollection<Restaurant>();
+        RegisterMongoCollection<Photo>();
+    }
+
+    private void RegisterMongoCollection<TCollection>()
+    {
+        _serviceCollection.AddTransient<IMongoCollection<TCollection>>(
+            provider =>
+            {
+                var database = provider.GetRequiredService<IMongoDatabase>();
+                return database.GetCollection<TCollection>(typeof(TCollection).Name);
+            });
+    }
+}
--- src/BurgerHub.Api/Program.cs
+++ src/BurgerHub.Api/Program.cs
@@ -1,8 +1,16 @@
+using Bogus;
+using BurgerHub.Api.Infrastructure;
+
+Randomizer.Seed = new Random(1337);
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var registry = new IocRegistry(builder.Services);
+registry.Register();
 
 var app = builder.Build();
 
