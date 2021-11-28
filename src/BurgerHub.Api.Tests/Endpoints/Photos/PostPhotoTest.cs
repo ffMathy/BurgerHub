eed8e7faaaf82c687b5d389b30632d7ad91f0521
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-11-28 18:24:40 +0100

    added test boilerplates.

---
+++ src/BurgerHub.Api.Tests/Endpoints/Photos/PostPhotoTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BurgerHub.Api.Tests.Endpoints.Photos;
+
+[TestClass]
+public class PostPhotoTest
+{
+}
---
+++ src/BurgerHub.Api.Tests/Endpoints/Restaurants/Reviews/PostReviewForRestaurantTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BurgerHub.Api.Tests.Endpoints.Restaurants.Reviews;
+
+[TestClass]
+public class PostReviewForRestaurantTest
+{
+}
