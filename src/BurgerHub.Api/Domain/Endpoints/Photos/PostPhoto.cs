5a5e6789d95dad3969bc05c22fb274aeba611d36
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-11-28 03:31:36 +0100

    changed picture into photo for consistency.

---
+++ src/BurgerHub.Api/Domain/Endpoints/Photos/PostPhoto.cs
@@ -0,0 +1,33 @@
+﻿using Ardalis.ApiEndpoints;
+using BurgerHub.Api.Domain.Models;
+using BurgerHub.Api.Infrastructure.Security.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace BurgerHub.Api.Domain.Endpoints.Photos;
+
+public record PostPhotoRequest(
+    IFormFile File);
+
+public class PostPhoto : BaseAsyncEndpoint
+    .WithRequest<PostPhotoRequest>
+    .WithoutResponse
+{
+    private readonly IMongoCollection<Photo> _photoCollection;
+
+    public PostPhoto(
+        IMongoCollection<Photo> photoCollection)
+    {
+        _photoCollection = photoCollection;
+    }
+    
+    [HttpPost("api/photos")]
+    [Authorize(Roles = AuthRoles.User)]
+    public override Task<ActionResult> HandleAsync(
+        PostPhotoRequest request, 
+        CancellationToken cancellationToken = new())
+    {
+        throw new NotImplementedException();
+    }
+}
